e4bd3f8a5fb7d08294f0b52db4eb9fa6477d8f1e
Reed Masek <masek014@ahl01.agate.msi.umn.edu>
2025-05-21 17:36:39 -0500

    Fixed bug in fitting within chip gap, where radius was always decremented at least once

--- nustar_tools/mapping/maps.py
+++ nustar_tools/mapping/maps.py
@@ -480,6 +480,8 @@ class FOV():
         det_map = make_det_map(self.evt_data, self.hdr)
         dets = mtools.find_dets_in_region(
             det_map, region.to_pixel(det_map.wcs))
+        if -1 in dets:
+            dets.remove(-1)
         while len(dets) > 1 and region.radius.value > 25:
             # Decrement by 2.5 arcseconds per iteration since
             # each detector pixel is about 2.5 arcseconds
